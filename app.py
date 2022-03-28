bb7d7471e4c27e61e66e2906e8172e6f7d3b21d5
zeeking99 <malik99jeelani@gmail.com>
2022-03-28 23:44:23 +0530

    error with empty response object

--- app.py
+++ app.py
@@ -7,11 +7,15 @@ app = Flask(__name__)
 c1 = Chatbot()
 
 @app.route("/send", methods=['GET', "POST"])
-
 def flask_chatbot_response():
-    msg = request.get_json()
-    print(msg)
-    return jsonify({ 'val': c1.chat_response(msg['val']) })
+    message = request.get_data()
+    message = message.decode()
+
+    response =  c1.chat_response(message)
+    response = jsonify({ 'val': response }) 
+    
+    #print(response)
+    return response 
 
 if __name__ == "__main__":
     app.run()
