865190d08bb2292e03ec403e1e46e8786ba42d00
zeeking99 <malik99jeelani@gmail.com>
2022-05-23 21:12:38 +0530

    Created new component Chatcontainer

--- frontend_interface/src/App.js
+++ frontend_interface/src/App.js
@@ -1,115 +1,155 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import Chat, { Bubble,  Icon,  IconButton,  useMessages } from "@chatui/core";
 import "@chatui/core/dist/index.css";
 import "./index.css";
 import SplitPane from "react-split-pane";
-//import Dictaphone from "./Dictaphone";
 import API from "./APIcall"
 import MyImageCaptureComponent from "./imagecapture";
 import ChatSDK from "@chatui/core";
 //import './chatui-theme.css';
+import SpeechRecognition, {
+  useSpeechRecognition
+} from "react-speech-recognition";
+import Chatcontainer from "./Chatcontainer";
 
-const initialMessages = [
-  {
-    type: 'text',
-    content: { text: 'Hello, This is Digital Receptionist Sam' },
-    user: { avatar: 'http://127.0.0.1:5500/frontend_interface/src/logo.svg'}
-  },
-];
-
-export default function App() {
-  const { messages, appendMsg, setTyping } = useMessages(initialMessages);
-
-  function handleSend(type, val) 
-  {
-    if (type === "text" && val.trim()) {
-      appendMsg({
-        type: "text",
-        content: { text: val },
-        position: "right",
-        user :{ avatar: 'http://127.0.0.1:5500/frontend_interface/src/user.svg' }
-      });
+//const initialMessages = [
+//  {
+//    type: 'text',
+//    content: { text: 'Hello, This is Digital Receptionist Sam' },
+//    user: { avatar: 'http://127.0.0.1:5500/frontend_interface/src/logo.svg'}
+//  },
+//];
 //
-      let response = API(val)
-
+//export default function App() {
+//  const { messages, appendMsg, setTyping } = useMessages(initialMessages);
 //
-      //response.then(function(res) { return res[]})    
-      setTyping(true);
+//  const [micOn, setMic] = useState(false)
 //
-      setTimeout(async () => {
-        const data = await response 
-        appendMsg({
-          type: "text",
-          content: { text: data },
-          user: { avatar: 'http://127.0.0.1:5500/frontend_interface/src/logo.svg' },
-        });
-      }, 1000);
-    }
-  }
+//  const {
+//      transcript,
+//      listening,
+//      resetTranscript,
+//      finalTranscript,
+//      browserSupportsSpeechRecognition
+//    } = useSpeechRecognition();
 //
-  function renderMessageContent(msg) {
-    const { content } = msg;
-    return <Bubble content={content.text} />;
-  }
-  
-/*
-  new ChatSDK({
-    config: {
-      toolbar: [
-        {
-          type: 'speech',
-          icon: 'mic',
-          title: 'Hold to Speak'
-        }
-      ]
-    },
-    handlers: {
-      onToolbarClick: function (item, ctx) {
-        if (item.type === 'speech') {
-          // 这里改成 App 提供的 bridge 方法
-          nativeInvoke('speech', (text) => {
-            if (text) {
-              // 通过 setText 更新输入框内容
-              bot.appRef.current.setText(text);
-            }
-          });
-        }
-      }
-    }
-  }); 
-  */
+//  if (!browserSupportsSpeechRecognition) {
+//    return <span>Browser doesn't support speech recognition.</span>;
+//  }
+//
+//  function handleSend(type, val) 
+//  {
+//    if (type === "text" && val.trim()) {
+//      appendMsg({
+//        type: "text",
+//        content: { text: val },
+//        position: "right",
+//        user :{ avatar: 'http://127.0.0.1:5500/frontend_interface/src/user.svg' }
+//      });
+////
+//      let response = API(val) // making the API call to receive a response from the bot.
+//
+////
+//      //response.then(function(res) { return res[]})    
+//      setTyping(true);
+////
+//      setTimeout(async () => {
+//        const data = await response 
+//        appendMsg({
+//          type: "text",
+//          content: { text: data },
+//          user: { avatar: 'http://127.0.0.1:5500/frontend_interface/src/logo.svg' },
+//        });
+//      }, 1000);
+//    }
+//  }
+////
+//  function renderMessageContent(msg) {
+//    const { content } = msg;
+//    return <Bubble content={content.text} />;
+//  }
+//
+//  function onToolbarClick(item)
+//  {
+//    if (item.type == 'speech' && micOn == false)
+//    {
+//      setMic(true)
+//      SpeechRecognition.startListening({continuous: true})
+//    }
+//    else
+//    {
+//      setMic(false)
+//      SpeechRecognition.stopListening()
+//
+//
+//      handleSend({type: 'text', val:"Hello"})
+//    }
+//  }
+//  
+///*
+//  new ChatSDK({
+//    config: {
+//      toolbar: [
+//        {
+//          type: 'speech',
+//          icon: 'mic',
+//          title: 'Hold to Speak'
+//        }
+//      ]
+//    },
+//    handlers: {
+//      onToolbarClick: function (item, ctx) {
+//        if (item.type === 'speech') {
+//          // 这里改成 App 提供的 bridge 方法
+//          nativeInvoke('speech', (text) => {
+//            if (text) {
+//              // 通过 setText 更新输入框内容
+//              bot.appRef.current.setText(text);
+//            }
+//          });
+//        }
+//      }
+//    }
+//  }); 
+//  */
+//
+//
+//
+//  return (
+//    <SplitPane split='vertical'  defaultSize={450} minSize={450} maxSize={650}> 
+//      <Chat
+//        locale="en-US"
+//        placeholder="Type here..."
+//        inputType="text"
+//        inputOptions={"voice"}
+//        navbar={{ title: "Digital Receptionist Sam" }}
+//        messages={messages}
+//        renderMessageContent={renderMessageContent}
+//        onToolbarClick={onToolbarClick}
+//        onSend={handleSend}   
+//        toolbar={[
+//          {
+//            type: "speech",
+//            title: "Speak",
+//            img: "http://localhost:5500/frontend_interface/src/voice.png",
+//          }
+//        ]}
+//      />
+//
+//
+//      {/* <div> Digital Human
+//          {/* <img src="http://localhost:5500/frontend_interface/src/digi_receptionist.png" alt='lol' height={700} width={800} class='center'/>  }
+//      </div> */}
+//
+//    <MyImageCaptureComponent/> 
+//
+//    </SplitPane>  
+//    
+//  );
+//}
+//
 
-
-
-  return (
-    <SplitPane split='vertical'  defaultSize={450} minSize={450} maxSize={650}> 
-      <Chat
-        locale="en-US"
-        placeholder="Type here..."
-        inputType="text"
-        inputOptions={"text"}
-        navbar={{ title: "Digital Receptionist Sam" }}
-        messages={messages}
-        renderMessageContent={renderMessageContent}
-        onSend={handleSend}   
-        recorder={{ canRecord: true }}
-        toolbar={[
-          {
-            type: "speech",
-            title: "Speak",
-            img: "http://localhost:5500/frontend_interface/src/voice.png",
-          }
-        ]}
-      />
-
-
-      {/* <div> Digital Human
-          {/* <img src="http://localhost:5500/frontend_interface/src/digi_receptionist.png" alt='lol' height={700} width={800} class='center'/>  }
-      </div> */}
-
-    <MyImageCaptureComponent/> 
-
-    </SplitPane>  
-    
-  );
+export default function App()
+{
+  return ( <Chatcontainer/> );
 }
